a713e9fb26b903f49fc4f6c72fabe29c0221935f
Mark Suter <msuter@live.elida.k12.oh.us>
2021-12-22 12:15:59 -0500

    anims set up, no triggers or context msg yet

--- Assets/ScriptsbySuter/AnimationBoss.cs
+++ Assets/ScriptsbySuter/AnimationBoss.cs
@@ -6,22 +6,29 @@ public class AnimationBoss : MonoBehaviour
 {
     public GameObject theSpeedplate;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Keypad1))
-        {
-            //theSpeedplate.GetComponent<Animator>().Play("1_PinsOut");
-            Debug.Log("1 pressed");
-        }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Keypad1))
+    //    {
+    //        //theSpeedplate.GetComponent<Animator>().Play("1_PinsOut");
+    //        Debug.Log("1 pressed");
+    //    }
 
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            theSpeedplate.GetComponent<Animator>().Play("2_LeftRightHalvesExpand");
-        }
-    }
+    //    if (Input.GetKeyDown(KeyCode.Keypad2))
+    //    {
+    //        theSpeedplate.GetComponent<Animator>().Play("2_LeftRightHalvesExpand");
+    //    }
+    //}
 
     public void Play1Pins()
     {
         theSpeedplate.GetComponent<Animator>().Play("1_PinsOut");
+        Debug.Log("1_PinsOut Played");
+    }
+
+    public void Play2Expand()
+    {
+        theSpeedplate.GetComponent<Animator>().Play("2_LeftRightHalvesExpand");
+        Debug.Log("2 expand played");
     }
 }
